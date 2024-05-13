fd1cae4db79914ec2ac9679838a6c180dab4d3d2
KoshalParwan <koshal.parwan@fasst.io>
2024-05-13 13:24:13 +0400

    feat: useTransition

--- app/about/error.js
+++ app/about/error.js
@@ -1,18 +1,27 @@
 'use client' // Error components must be Client Components
  
-import { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+import { useTransition, useEffect } from 'react'
  
 export default function Error({ error, reset }) {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
   }, [error])
- 
+  
   return (
-    <div>
+    <div className={`${isPending ? 'opacity-5' : 'opacity-100'}`}>
       <h2>Something went wrong!</h2>
       {/* Attempt to recover by trying to re-render the segment */}
-      <button type='button' onClick={() => reset()}>
+      <button type='button' onClick={() => {
+        startTransition(() => {
+          router.refresh();
+          reset();
+        });
+      }}>
         Try again
       </button>
     </div>
--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,6 @@
 const AboutPage = () => {
+  if (Math.random() > 0.5) throw new Error('ooops');
+
   return (
     <div>About Page</div>
   )
