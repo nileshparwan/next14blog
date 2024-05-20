7d3d10161fb48d84e7eafeb8f0f8b5c49dc29630
KoshalParwan <koshal.parwan@fasst.io>
2024-05-20 13:01:07 +0400

    feat: dynamic metadata

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,8 +1,17 @@
 import { notFound } from 'next/navigation';
 
-export const metadata = {
-  title: "Blog",
-};
+const titles = {
+  first: 'hello First!',
+  second: 'Hello Second!'
+}
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  const description = (await parent).description ?? 'Default description';
+  return {
+    title: titles[params.slug],
+    description
+  };
+}
 
 export default function BlogPage({ params }) {
 
